21c0a9be0ada4e7677b8ea8eaa3e8f0b93e1a7a4
Bogdan <bogdanrozumovskij@gmail.com>
2019-11-20 22:51:08 +0200

    Fix auth bag

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -45,10 +45,10 @@ export const auth = (email, password, isSignup) => {
             password: password,
             returnSecureToken: true
         }
-        let url = 'https://www.googleapis.com/identitytoolkit/v3/relyingparty/signupNewUser?key=AIzaSyDSvoYqDXGAZ-i40eGFKLX34TOBO8Otmic';
-        if(!isSignup) {
-            url = 'https://www.googleapis.com/identitytoolkit/v3/relyingparty/verifyPassword?key=AIzaSyDSvoYqDXGAZ-i40eGFKLX34TOBO8Otmic'
-        }
+        let sign = !isSignup ? 'signInWithPassword' : 'signUp'
+
+        let url = `https://identitytoolkit.googleapis.com/v1/accounts:${sign}?key=AIzaSyDSvoYqDXGAZ-i40eGFKLX34TOBO8Otmic`;
+        
         axios.post(url, authData)
             .then(response => {
                 dispatch(authSuccess(response.data.token, response.data.localId));
